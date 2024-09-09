c8fc0e67e4dbac8d873063455cecfd2c7e2e9c22
DiedeKlink <diede@protonmail.com>
2024-09-09 13:35:19 +0200

    Basic Validation

--- src/Textarea.jsx
+++ src/Textarea.jsx
@@ -1,18 +1,29 @@
 import { useState } from "react";
+import Warning from "./Warning";
 
 export default function Textarea() {
   const [text, setText] = useState("");
+  const [warning, setWarning] = useState(false);
 
+  const handleChange = (e) => {
+    let newText = e.target.value;
+
+    if (newText.includes("<script>")) {
+      setWarning(true);
+      newText = newText.replace("<script>", "");
+    }
+
+    setText(newText);
+  };
   return (
-    <textarea
-      value={text}
-      onChange={(e) => {
-        const newText = e.target.value;
-        setText(newText);
-      }}
-      className="textarea"
-      placeholder="Enter your text"
-      spellCheck="false"
-    />
+    <div className="textarea">
+      <textarea
+        value={text}
+        onChange={handleChange}
+        placeholder="Enter your text"
+        spellCheck="false"
+      />
+      {warning && <Warning />}
+    </div>
   );
 }
