b77e6ed26cf6efe8f6c9169b6450af3b8c5f1b81
DiedeKlink <diede@protonmail.com>
2024-09-09 12:41:53 +0200

    Change Event

--- src/Textarea.jsx
+++ src/Textarea.jsx
@@ -1,3 +1,13 @@
 export default function Textarea() {
-  return <textarea className="textarea" />;
+  return (
+    <textarea
+      onChange={(e) => {
+        console.log("Changed!");
+        console.log(e.target.value);
+      }}
+      className="textarea"
+      placeholder="Enter your text"
+      spellCheck="false"
+    />
+  );
 }
